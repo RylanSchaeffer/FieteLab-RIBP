eb0abaf0a705341a8c83e87583ecce7052c35bb8
Rylan <rylanschaeffer@gmail.com>
2021-06-01 21:19:12 -0700

    Updated IBP prior

--- exp_00_ibp_prior/main.py
+++ exp_00_ibp_prior/main.py
@@ -1,14 +1,15 @@
 # TODO: switch to log colorscale scaling
+from itertools import product
 import numpy as np
 import os
 import scipy.stats
+from typing import List
 
 import plot
 from utils.data import vectorized_sample_sequence_from_ibp
 
 
 def main():
-
     # set seed
     np.random.seed(1)
 
@@ -19,112 +20,129 @@ def main():
 
     T = 50  # max time
     num_samples = 5000  # number of samples to draw from IBP(alpha)
-    alphas = [1.1, 10.37, 15.78, 30.91]  # IBP parameter
-    # alphas = [30.91]  # IBP parameter
+    alphas = [2.64, 30.91]  # 15.78,
+    # don't need to be the same
+    betas = [3.11, 13.82]  # , 24.5
 
-    sampled_customers_dishes_by_alpha = sample_from_ibp(
+    alphas = [30.91]
+    betas = [3.11]
+
+    sampled_customers_dishes_by_alpha_beta = sample_from_ibp(
         T=T,
         alphas=alphas,
+        betas=betas,
         exp_dir=exp_dir,
         num_samples=num_samples)
 
-    analytical_dish_distribution_poisson_rate_by_alpha_by_T = construct_analytical_indian_dish_distribution(
-        T=T,
-        alphas=alphas)
-
-    analytical_customers_dishes_by_alpha = construct_analytical_ibp(
+    analytical_dish_distribution_poisson_rate_by_alpha_beta_by_T = construct_analytical_indian_dish_distribution(
         T=T,
         alphas=alphas,
+        betas=betas)
+
+    analytical_customers_dishes_by_alpha_beta = construct_analytical_ibp(
+        T=T,
+        alphas=alphas,
+        betas=betas,
         exp_dir=exp_dir)
 
     plot.plot_indian_buffet_num_dishes_dist_by_customer_num(
-        analytical_dish_distribution_poisson_rate_by_alpha_by_T=analytical_dish_distribution_poisson_rate_by_alpha_by_T,
+        analytical_dish_distribution_poisson_rate_by_alpha_by_T=analytical_dish_distribution_poisson_rate_by_alpha_beta_by_T,
         plot_dir=plot_dir)
 
     plot.plot_recursion_visualization(
-        analytical_customers_dishes_by_alpha=analytical_customers_dishes_by_alpha,
-        analytical_dish_distribution_poisson_rate_by_alpha_by_T=analytical_dish_distribution_poisson_rate_by_alpha_by_T,
+        analytical_customers_dishes_by_alpha_beta=analytical_customers_dishes_by_alpha_beta,
+        analytical_dish_distribution_poisson_rate_by_alpha_by_T=analytical_dish_distribution_poisson_rate_by_alpha_beta_by_T,
         plot_dir=plot_dir)
-
     plot.plot_analytics_vs_monte_carlo_customer_dishes(
-        sampled_customers_dishes_by_alpha=sampled_customers_dishes_by_alpha,
-        analytical_customers_dishes_by_alpha=analytical_customers_dishes_by_alpha,
+        sampled_customers_dishes_by_alpha_beta=sampled_customers_dishes_by_alpha_beta,
+        analytical_customers_dishes_by_alpha_beta=analytical_customers_dishes_by_alpha_beta,
         plot_dir=plot_dir)
 
-    num_reps = 10
-    error_means_per_num_samples_per_alpha, error_sems_per_num_samples_per_alpha = \
-        calc_analytical_vs_monte_carlo_mse(
-            T=T,
-            alphas=alphas,
-            exp_dir=exp_dir,
-            num_reps=num_reps,
-            num_samples=num_samples,
-            analytical_customer_dishes_by_alpha=analytical_customers_dishes_by_alpha)
-
-    plot.plot_analytical_vs_monte_carlo_mse(
-        error_means_per_num_samples_per_alpha=error_means_per_num_samples_per_alpha,
-        error_sems_per_num_samples_per_alpha=error_sems_per_num_samples_per_alpha,
-        num_reps=num_reps,
-        plot_dir=plot_dir)
+    # num_reps = 2
+    # error_means_per_num_samples_per_alpha_beta, error_sems_per_num_samples_per_alpha_beta = \
+    #     calc_analytical_vs_monte_carlo_mse(
+    #         T=T,
+    #         alphas=alphas,
+    #         betas=betas,
+    #         exp_dir=exp_dir,
+    #         num_reps=num_reps,
+    #         num_samples=num_samples,
+    #         analytical_customer_dishes_by_alpha_beta=analytical_customers_dishes_by_alpha_beta)
+    #
+    # plot.plot_analytical_vs_monte_carlo_mse(
+    #     error_means_per_num_samples_per_alpha=error_means_per_num_samples_per_alpha_beta,
+    #     error_sems_per_num_samples_per_alpha=error_sems_per_num_samples_per_alpha_beta,
+    #     num_reps=num_reps,
+    #     plot_dir=plot_dir)
 
 
 def calc_analytical_vs_monte_carlo_mse(T: int,
-                                       alphas,
+                                       alphas: List[float],
+                                       betas: List[float],
                                        exp_dir,
                                        num_reps: int,
                                        num_samples: int,
-                                       analytical_customer_dishes_by_alpha):
+                                       analytical_customer_dishes_by_alpha_beta):
+    # sample_subset_sizes = np.logspace(1, np.log10(num_samples), 5).astype(np.int)
+    sample_subset_sizes = np.logspace(1, 3, 5).astype(np.int)
 
-    sample_subset_sizes = np.logspace(1, np.log10(num_samples), 5).astype(np.int)
-
-    rep_errors = np.zeros(shape=(num_reps, len(alphas), len(sample_subset_sizes)))
+    num_alpha_beta_pairs = len(list(product(alphas, betas)))
+    rep_errors = np.zeros(shape=(num_reps, num_alpha_beta_pairs, len(sample_subset_sizes)))
 
     for rep_idx in range(num_reps):
         # draw sample from CRP
-        sampled_customer_dishes_by_alpha = sample_from_ibp(
+        sampled_customer_dishes_by_alpha_beta = sample_from_ibp(
             T=T,
             alphas=alphas,
+            betas=betas,
             exp_dir=exp_dir,
             num_samples=num_samples,
             rep_idx=rep_idx)
 
-        for alpha_idx, alpha in enumerate(alphas):
+        for alpha_beta_pair_idx, (alpha, beta) in enumerate(product(alphas, betas)):
             # for each subset of data, calculate the error
+            alpha_beta_key = rf'$\alpha={alpha}, \beta={beta}$'
             for sample_idx, num_samples in enumerate(sample_subset_sizes):
+                monte_carlo_estimate = np.mean(
+                    sampled_customer_dishes_by_alpha_beta[alpha_beta_key][:num_samples],
+                    axis=0)
                 diff = np.subtract(
-                        np.mean(sampled_customer_dishes_by_alpha[alpha][:num_samples],
-                                axis=0),
-                        analytical_customer_dishes_by_alpha[alpha])
+                    monte_carlo_estimate,
+                    analytical_customer_dishes_by_alpha_beta[alpha_beta_key])
                 rep_error = np.square(np.linalg.norm(diff[~np.isnan(diff)]))
-                rep_errors[rep_idx, alpha_idx, sample_idx] = rep_error
+                rep_errors[rep_idx, alpha_beta_pair_idx, sample_idx] = rep_error
 
-    mean_errors_per_num_samples_per_alpha, sem_errors_per_num_samples_per_alpha = {}, {}
-    for alpha_idx, alpha in enumerate(alphas):
-        mean_errors_per_num_samples_per_alpha[alpha] = {
+    mean_errors_per_num_samples_per_alpha_beta, sem_errors_per_num_samples_per_alpha_beta = {}, {}
+    for alpha_beta_pair_idx, (alpha, beta) in enumerate(product(alphas, betas)):
+        alpha_beta_key = rf'$\alpha={alpha}, \beta={beta}$'
+        mean_errors_per_num_samples_per_alpha_beta[alpha_beta_key] = {
             num_sample: error for num_sample, error in
-            zip(sample_subset_sizes, np.mean(rep_errors[:, alpha_idx, :], axis=0))}
-        sem_errors_per_num_samples_per_alpha[alpha] = {
+            zip(sample_subset_sizes, np.mean(rep_errors[:, alpha_beta_pair_idx, :], axis=0))}
+        sem_errors_per_num_samples_per_alpha_beta[alpha_beta_key] = {
             num_sample: error for num_sample, error in
-            zip(sample_subset_sizes, scipy.stats.sem(rep_errors[:, alpha_idx, :], axis=0))}
+            zip(sample_subset_sizes, scipy.stats.sem(rep_errors[:, alpha_beta_pair_idx, :], axis=0))}
 
-    return mean_errors_per_num_samples_per_alpha, sem_errors_per_num_samples_per_alpha
+    return mean_errors_per_num_samples_per_alpha_beta, sem_errors_per_num_samples_per_alpha_beta
 
 
-def construct_analytical_customers_dishes(T, alpha):
-
+def construct_analytical_customers_dishes(T: int,
+                                          alpha: float,
+                                          beta: float):
     # shape: (number of customers, number of dishes)
     assert alpha > 0
+    assert beta > 0
     alpha = float(alpha)
+    beta = float(beta)
 
     # heuristic: 3 * expected number
     # needs to match whatever the sampled IBP max is, otherwise shapes disagree
-    max_dishes = int(3 * alpha * np.sum(1 / (1 + np.arange(T))))
+    max_dishes = int(3 * alpha * beta * np.sum(1 / (1 + np.arange(T))))
     analytical_customers_dishes = np.zeros(shape=(T + 1, max_dishes + 1))
     analytical_customer_dishes_running_sum = np.zeros(shape=(T + 1, max_dishes + 1))
     dish_indices = np.arange(max_dishes + 1)
 
     # customer 1 samples only new dishes
-    new_dishes_rate = alpha / 1
+    new_dishes_rate = alpha / (beta + 1 - 1)
     analytical_customers_dishes[1, :] = np.cumsum(scipy.stats.poisson.pmf(dish_indices[::-1], mu=new_dishes_rate))[
                                         ::-1]
     analytical_customer_dishes_running_sum[1, :] = analytical_customers_dishes[1, :]
@@ -132,9 +150,9 @@ def construct_analytical_customers_dishes(T, alpha):
 
     # all subsequent customers sample new dishes
     for customer_num in range(2, T + 1):
-        analytical_customers_dishes[customer_num, :] = analytical_customer_dishes_running_sum[customer_num - 1,
-                                                       :] / customer_num
-        new_dishes_rate = alpha / customer_num
+        analytical_customers_dishes[customer_num, :] = \
+            analytical_customer_dishes_running_sum[customer_num - 1, :] / (beta + customer_num)
+        new_dishes_rate = alpha * beta / (beta + customer_num - 1)
         cdf_lambda_t_minus_1 = scipy.stats.poisson.cdf(dish_indices, mu=total_dishes_rate_running_sum)
         cdf_lambda_t = scipy.stats.poisson.cdf(dish_indices, mu=total_dishes_rate_running_sum + new_dishes_rate)
         cdf_diff = np.subtract(cdf_lambda_t_minus_1, cdf_lambda_t)
@@ -147,10 +165,13 @@ def construct_analytical_customers_dishes(T, alpha):
     return analytical_customers_dishes[1:, 1:]
 
 
-def construct_analytical_ibp(T, alphas, exp_dir):
+def construct_analytical_ibp(T: int,
+                             alphas: List[float],
+                             betas: List[float],
+                             exp_dir):
     analytical_customers_dishes_by_alpha = {}
-    for alpha in alphas:
-        ibp_analytics_path = os.path.join(exp_dir, f'ibp_analytics_{alpha}.npz')
+    for alpha, beta in product(alphas, betas):
+        ibp_analytics_path = os.path.join(exp_dir, f'ibp_analytics_a={alpha}_b={beta}.npz')
         if os.path.isfile(ibp_analytics_path):
             npz_file = np.load(ibp_analytics_path)
             analytical_customers_dishes = npz_file['analytical_customers_dishes']
@@ -158,53 +179,57 @@ def construct_analytical_ibp(T, alphas, exp_dir):
         else:
             analytical_customers_dishes = construct_analytical_customers_dishes(
                 T=T,
-                alpha=alpha)
+                alpha=alpha,
+                beta=beta)
             np.savez(analytical_customers_dishes=analytical_customers_dishes,
                      file=ibp_analytics_path)
-        analytical_customers_dishes_by_alpha[alpha] = analytical_customers_dishes
+        analytical_customers_dishes_by_alpha[rf'$\alpha={alpha}, \beta={beta}$'] = analytical_customers_dishes
     return analytical_customers_dishes_by_alpha
 
 
-def construct_analytical_indian_dish_distribution(T, alphas):
-
+def construct_analytical_indian_dish_distribution(T: int,
+                                                  alphas: List[float],
+                                                  betas: List[float]):
     num_customers = 1 + np.arange(T)
     dish_distribution_poisson_rate_by_alpha_by_T = {}
-    for alpha in alphas:
-        dish_distribution_poisson_rate_by_alpha_by_T[alpha] = {}
+    for alpha, beta in product(alphas, betas):
+        key = rf'$\alpha={alpha}, \beta={beta}$'
+        dish_distribution_poisson_rate_by_alpha_by_T[key] = {}
         poisson_rate = 0
         for t in num_customers:
-            poisson_rate += alpha / t
-            dish_distribution_poisson_rate_by_alpha_by_T[alpha][t] = poisson_rate
+            poisson_rate += alpha * beta / (beta + t - 1)
+            dish_distribution_poisson_rate_by_alpha_by_T[key][t] = poisson_rate
     return dish_distribution_poisson_rate_by_alpha_by_T
 
 
-def sample_from_ibp(T,
-                    alphas,
-                    exp_dir,
-                    num_samples,
+def sample_from_ibp(T: int,
+                    alphas: List[float],
+                    betas: List[float],
+                    exp_dir: str,
+                    num_samples: int,
                     rep_idx=0):
-
     # generate Monte Carlo samples from IBP(alpha)
-    sampled_customer_dishes_by_alpha = {}
-    for alpha in alphas:
-        ibp_samples_path = os.path.join(exp_dir, f'ibp_sample_{alpha}_rep_idx={rep_idx}.npz')
+    sampled_customer_dishes_by_alpha_beta = {}
+    for alpha, beta in product(alphas, betas):
+        ibp_samples_path = os.path.join(exp_dir, f'ibp_sample_a={alpha}_b={beta}_rep_idx={rep_idx}.npz')
         if os.path.isfile(ibp_samples_path):
             ibp_sample_data = np.load(ibp_samples_path)
             sampled_customer_dishes = ibp_sample_data['sampled_customer_dishes']
-            assert sampled_customer_dishes.shape[0] == num_samples
+            assert sampled_customer_dishes.shape[0] >= num_samples
             assert sampled_customer_dishes.shape[1] == T
             print(f'Loaded samples for {ibp_samples_path}')
         else:
             sampled_customer_dishes = vectorized_sample_sequence_from_ibp(
-                T=np.full(shape=num_samples, fill_value=T),
-                alpha=np.full(shape=num_samples, fill_value=alpha))
+                T=np.full(shape=5000, fill_value=T),
+                alpha=np.full(shape=5000, fill_value=alpha),
+                beta=np.full(shape=5000, fill_value=beta))
             sampled_customer_dishes = np.stack(sampled_customer_dishes)
             np.savez(file=ibp_samples_path,
                      sampled_customer_dishes=sampled_customer_dishes)
             print(f'Generated samples for {ibp_samples_path}')
-        sampled_customer_dishes_by_alpha[alpha] = sampled_customer_dishes
+        sampled_customer_dishes_by_alpha_beta[rf'$\alpha={alpha}, \beta={beta}$'] = sampled_customer_dishes
 
-    return sampled_customer_dishes_by_alpha
+    return sampled_customer_dishes_by_alpha_beta
 
 
 if __name__ == '__main__':
--- exp_00_ibp_prior/plot.py
+++ exp_00_ibp_prior/plot.py
@@ -15,16 +15,18 @@ alphas_color_map = {
 }
 
 
-def plot_analytics_vs_monte_carlo_customer_dishes(sampled_customers_dishes_by_alpha,
-                                                  analytical_customers_dishes_by_alpha,
+def plot_analytics_vs_monte_carlo_customer_dishes(sampled_customers_dishes_by_alpha_beta,
+                                                  analytical_customers_dishes_by_alpha_beta,
                                                   plot_dir):
     # plot analytics versus monte carlo estimates
-    for alpha in sampled_customers_dishes_by_alpha.keys():
+    for alpha_and_beta in sampled_customers_dishes_by_alpha_beta.keys():
         fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
 
-        sampled_customer_tables = sampled_customers_dishes_by_alpha[alpha]
+        sampled_customer_tables = sampled_customers_dishes_by_alpha_beta[alpha_and_beta]
         average_customer_tables = np.mean(sampled_customer_tables, axis=0)
-        analytical_customer_dishes = analytical_customers_dishes_by_alpha[alpha]
+        analytical_customer_dishes = analytical_customers_dishes_by_alpha_beta[alpha_and_beta]
+
+        max_sampled_table = np.argmax(np.sum(average_customer_tables, axis=0) == 0)
 
         # replace 0s with nans to allow for log scaling
         average_customer_tables[average_customer_tables == 0.] = np.nan
@@ -32,30 +34,36 @@ def plot_analytics_vs_monte_carlo_customer_dishes(sampled_customers_dishes_by_al
         analytical_customer_dishes[analytical_customer_dishes < cutoff] = np.nan
 
         ax = axes[0]
-        sns.heatmap(average_customer_tables,
+        sns.heatmap(average_customer_tables[:, :max_sampled_table],
                     ax=ax,
-                    mask=np.isnan(average_customer_tables),
+                    mask=np.isnan(average_customer_tables)[:, :max_sampled_table],
                     cmap='jet',
-                    norm=LogNorm(vmin=cutoff, vmax=1., ),
+                    vmin=cutoff,
+                    vmax=1.,
+                    norm=LogNorm(),
                     # cbar_kws=dict(label='$p(z_{tk}=1)$'),
                     )
 
-        ax.set_title(r'Monte Carlo  $p(z_{tk} = 1 | $' + rf'$\alpha$={alpha})')
+        ax.set_title(r'Monte Carlo  $p(z_{tk} = 1 | $' + alpha_and_beta + ')')
         ax.set_ylabel(r'Customer Index')
         ax.set_xlabel(r'Dish Index')
 
         ax = axes[1]
-        sns.heatmap(analytical_customer_dishes,
+        sns.heatmap(analytical_customer_dishes[:, :max_sampled_table],
                     ax=ax,
-                    mask=np.isnan(analytical_customer_dishes),
+                    mask=np.isnan(analytical_customer_dishes)[:, :max_sampled_table],
                     cmap='jet',
-                    norm=LogNorm(vmin=cutoff,
-                                 vmax=1., ),
+                    vmin=cutoff,
+                    vmax=1.,
+                    norm=LogNorm(),
                     )
-        ax.set_title(r'Analytical  $p(z_{tk} = 1 | $' + rf'$\alpha$={alpha})')
+        ax.set_title(r'Analytical  $p(z_{tk} = 1 | $' + alpha_and_beta + ')')
         ax.set_xlabel(r'Dish Index')
 
-        fig.savefig(os.path.join(plot_dir, f'analytics_vs_monte_carlo_customer_tables={alpha}.png'),
+        alpha_and_beta = alpha_and_beta.split('=')
+        alpha = alpha_and_beta[1].split(',')[0]
+        beta = alpha_and_beta[2][:-1]
+        fig.savefig(os.path.join(plot_dir, f'analytics_vs_monte_carlo_customer_tables_a={alpha}_b={beta}.png'),
                     bbox_inches='tight',
                     dpi=300)
         # plt.show()
@@ -67,15 +75,15 @@ def plot_analytical_vs_monte_carlo_mse(error_means_per_num_samples_per_alpha,
                                        num_reps,
                                        plot_dir):
 
-    alphas = list(error_sems_per_num_samples_per_alpha.keys())
+    alphas_and_betas = list(error_sems_per_num_samples_per_alpha.keys())
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 4))
-    for alpha in alphas:
-        ax.errorbar(x=list(error_means_per_num_samples_per_alpha[alpha].keys()),
-                    y=list(error_means_per_num_samples_per_alpha[alpha].values()),
-                    yerr=list(error_sems_per_num_samples_per_alpha[alpha].values()),
-                    label=rf'$\alpha$={alpha}',
-                    c=alphas_color_map[alpha])
+    for alpha_beta in alphas_and_betas:
+        ax.errorbar(x=list(error_means_per_num_samples_per_alpha[alpha_beta].keys()),
+                    y=list(error_means_per_num_samples_per_alpha[alpha_beta].values()),
+                    yerr=list(error_sems_per_num_samples_per_alpha[alpha_beta].values()),
+                    label=alpha_beta,
+                    )
     ax.legend(title=f'Num Repeats: {num_reps}')
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -92,16 +100,16 @@ def plot_analytical_vs_monte_carlo_mse(error_means_per_num_samples_per_alpha,
 def plot_indian_buffet_num_dishes_dist_by_customer_num(analytical_dish_distribution_poisson_rate_by_alpha_by_T,
                                                        plot_dir):
     # plot how the CRT table distribution changes for T customers
-    alphas = list(analytical_dish_distribution_poisson_rate_by_alpha_by_T.keys())
-    T = len(analytical_dish_distribution_poisson_rate_by_alpha_by_T[alphas[0]])
+    alphas_and_betas = list(analytical_dish_distribution_poisson_rate_by_alpha_by_T.keys())
+    T = len(analytical_dish_distribution_poisson_rate_by_alpha_by_T[alphas_and_betas[0]])
     customer_idx = 1 + np.arange(T)
     cmap = plt.get_cmap('jet_r')
 
-    for alpha in alphas:
+    for alpha_and_beta in alphas_and_betas:
         for t in customer_idx:
-            max_val = 2 * analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha][T]
+            max_val = 2 * analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha_and_beta][T]
             x = np.arange(0, max_val)
-            y = scipy.stats.poisson.pmf(x, mu=analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha][t])
+            y = scipy.stats.poisson.pmf(x, mu=analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha_and_beta][t])
             plt.plot(x,
                      y,
                      # label=f'T={t}',
@@ -115,24 +123,29 @@ def plot_indian_buffet_num_dishes_dist_by_customer_num(analytical_dish_distribut
                                 # boundaries=np.arange(-0.05, T + 0.1, .1)
                                 )
         colorbar.set_label('Number of Customers')
-        plt.title(fr'Indian Buffet Dish Distribution ($\alpha$={alpha})')
+        plt.title(fr'Indian Buffet Dish Distribution ({alpha_and_beta})')
         plt.xlabel(r'Number of Dishes after T Customers')
         plt.ylabel(r'P(Number of Dishes after T Customers)')
         plt.xlim(0., max_val)
-        plt.savefig(os.path.join(plot_dir, f'ibp_dish_distribution_alpha={alpha}.png'),
+
+        alpha_and_beta = alpha_and_beta.split('=')
+        alpha = alpha_and_beta[1].split(',')[0]
+        beta = alpha_and_beta[2][:-1]
+
+        plt.savefig(os.path.join(plot_dir, f'ibp_dish_distribution_a={alpha}_b={beta}.png'),
                     bbox_inches='tight',
                     dpi=300)
         # plt.show()
         plt.close()
 
 
-def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
+def plot_recursion_visualization(analytical_customers_dishes_by_alpha_beta,
                                  analytical_dish_distribution_poisson_rate_by_alpha_by_T,
                                  plot_dir):
-    alphas = list(analytical_customers_dishes_by_alpha.keys())
+    alphas_and_betas = list(analytical_customers_dishes_by_alpha_beta.keys())
     cutoff = 1e-8
 
-    for alpha in alphas:
+    for alpha_and_beta in alphas_and_betas:
         fig, axes = plt.subplots(nrows=1,
                                  ncols=5,
                                  figsize=(13, 4),
@@ -140,7 +153,7 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
                                  sharex=True)
 
         ax = axes[0]
-        cum_customer_dish_eating_probs = np.cumsum(analytical_customers_dishes_by_alpha[alpha], axis=0)
+        cum_customer_dish_eating_probs = np.cumsum(analytical_customers_dishes_by_alpha_beta[alpha_and_beta], axis=0)
         cum_customer_dish_eating_probs[cum_customer_dish_eating_probs < cutoff] = np.nan
 
         # figure out which dish falls below cutoff. if no dishes are below threshold, need to set
@@ -153,7 +166,8 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
             cbar_kws=dict(label=r'$\sum_{t^{\prime} = 1}^{t-1} p(z_{t^{\prime} k} = 1)$'),
             cmap='jet',
             mask=np.isnan(cum_customer_dish_eating_probs[:, :max_dish_idx]),
-            norm=LogNorm(vmin=cutoff),
+            vmin=cutoff,
+            norm=LogNorm(),
         )
         ax.set_xlabel('Dish Index')
         ax.set_ylabel('Customer Index')
@@ -165,8 +179,8 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
         ax = axes[2]
         table_distributions_by_T_array = np.stack([
             scipy.stats.poisson.pmf(np.arange(max_dish_idx),
-                                    mu=analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha][key])
-            for key in sorted(analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha].keys())])
+                                    mu=analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha_and_beta][key])
+            for key in sorted(analytical_dish_distribution_poisson_rate_by_alpha_by_T[alpha_and_beta].keys())])
         table_distributions_by_T_array[table_distributions_by_T_array < cutoff] = np.nan
         sns.heatmap(
             data=table_distributions_by_T_array[:, :max_dish_idx],
@@ -174,7 +188,8 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
             cbar_kws=dict(label=r'$p(\Lambda_t = \ell)$'),
             cmap='jet',
             mask=np.isnan(table_distributions_by_T_array[:, :max_dish_idx]),
-            norm=LogNorm(vmin=cutoff, ),
+            vmin=cutoff,
+            norm=LogNorm(),
         )
         ax.set_title('Distribution over\nNumber of Dishes')
         ax.set_xlabel('Dish Index')
@@ -183,7 +198,7 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
         axes[3].axis('off')
 
         ax = axes[4]
-        analytical_customer_dishes = np.copy(analytical_customers_dishes_by_alpha[alpha])
+        analytical_customer_dishes = np.copy(analytical_customers_dishes_by_alpha_beta[alpha_and_beta])
         analytical_customer_dishes[analytical_customer_dishes < cutoff] = np.nan
         sns.heatmap(
             data=analytical_customer_dishes[:, :max_dish_idx],
@@ -196,7 +211,10 @@ def plot_recursion_visualization(analytical_customers_dishes_by_alpha,
         ax.set_title('New Customer\'s Distribution')
         ax.set_xlabel('Dish Index')
 
-        fig.savefig(os.path.join(plot_dir, f'ibp_recursion_alpha={alpha}.png'),
+        alpha_and_beta = alpha_and_beta.split('=')
+        alpha = alpha_and_beta[1].split(',')[0]
+        beta = alpha_and_beta[2][:-1]
+        fig.savefig(os.path.join(plot_dir, f'ibp_recursion_a={alpha}_b={beta}.png'),
                     bbox_inches='tight',
                     dpi=300)
         # plt.show()
