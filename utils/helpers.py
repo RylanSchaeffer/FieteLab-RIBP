29c7c672b6ff3da8d70d6e65b16a6055b18ef59c
Rylan <rylanschaeffer@gmail.com>
2021-08-24 09:31:02 -0700

    Working on implementing linear Gaussian IBP

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,14 +2,9 @@ import numpy as np
 import torch
 
 
-def assert_numpy_no_nan_no_inf(x):
+def numpy_assert_no_nan_no_inf(x):
     assert np.all(~np.isnan(x))
     assert np.all(~np.isinf(x))
-
-
-def assert_torch_no_nan_no_inf(x):
-    assert torch.all(~torch.isnan(x))
-    assert torch.all(~torch.isinf(x))
 
 
 def numpy_logits_to_probs(logits):
@@ -22,8 +17,42 @@ def numpy_probs_to_logits(probs):
     return logits
 
 
-def torch_expected_log_bernoulli_prob(p_prob: torch.Tensor,
-                                      q_prob: torch.Tensor) -> torch.Tensor:
+def torch_assert_no_nan_no_inf(x):
+    assert torch.all(~torch.isnan(x))
+    assert torch.all(~torch.isinf(x))
+
+
+def torch_convert_half_cov_to_cov(half_cov: torch.Tensor) -> torch.Tensor:
+    """
+    Converts half-covariance M into covariance M^T M in a batched manner.
+    Convert batch half-covariance to covariance.
+
+    Assumes M is [batch size, D, D] tensor. If no batch dimension exists,
+    one will be added and then subsequently removed.
+    """
+
+    # if no batch dimension is given, add one
+    if len(half_cov.shape) == 2:
+        half_cov = torch.unsqueeze(half_cov, dim=0)
+
+    cov = torch.einsum(
+        'abc, abd->acd',
+        half_cov,
+        half_cov)
+    # batch_size = half_cov.shape[0]
+    # cov_test = torch.stack([torch.matmul(half_cov[k].T, half_cov[k])
+    #                         for k in range(batch_size)])
+    # assert torch.allclose(cov, cov_test)
+
+    # if no batch dimension was originally given, remove
+    if len(half_cov.shape) == 2:
+        cov = torch.squeeze(cov, dim=0)
+
+    return cov
+
+
+def torch_expected_log_bernoulli_under_bernoulli(p_prob: torch.Tensor,
+                                                 q_prob: torch.Tensor) -> torch.Tensor:
     """
     Compute E_{q(x)}[log p(x)] where p(x) and q(x)
 
@@ -40,10 +69,10 @@ def torch_expected_log_bernoulli_prob(p_prob: torch.Tensor,
     return total
 
 
-def torch_expected_log_gaussian_prob(p_mean: torch.Tensor,
-                                     p_cov: torch.Tensor,
-                                     q_mean: torch.Tensor,
-                                     q_cov: torch.Tensor) -> torch.Tensor:
+def torch_expected_log_gaussian_under_gaussian(p_mean: torch.Tensor,
+                                               p_cov: torch.Tensor,
+                                               q_mean: torch.Tensor,
+                                               q_cov: torch.Tensor) -> torch.Tensor:
     """
     Compute E_{q(x)}[log p(x)] where p(x) and q(x) are both Gaussian.
 
@@ -56,7 +85,8 @@ def torch_expected_log_gaussian_prob(p_mean: torch.Tensor,
     batch_dim = p_mean.shape[0]
 
     # sum_k -0.5 * log (2 pi |p_cov|)
-    term_one = -0.5 * torch.sum(2 * np.pi * torch.linalg.det(p_cov))
+    term_one = -0.5 * torch.sum(torch.log(2 * np.pi * torch.linalg.det(p_cov)))
+
     p_precision = torch.linalg.inv(p_cov)  # recall, precision = cov^{-1}
 
     # sum_k -0.5 Tr[p_precision (q_cov + q_mean q_mean^T)]
@@ -107,23 +137,97 @@ def torch_expected_log_gaussian_prob(p_mean: torch.Tensor,
     return total
 
 
-def torch_expected_log_likelihood(observation: torch.Tensor,
-                                  ) -> torch.Tensor:
-    raise NotImplementedError
+def torch_expected_log_gaussian_under_linear_gaussian(observation: torch.Tensor,
+                                                      q_A_mean: torch.Tensor,
+                                                      q_A_cov: torch.Tensor,
+                                                      q_Z_mean: torch.Tensor,
+                                                      sigma_obs: float = 1.) -> torch.Tensor:
+    """
+    Compute E_{q(A, Z)}[log p(x|A, Z)] where p(x) = Gaussian(Z @ A, sigma_obs^2 I)
+    and q(A, Z) = q(A) q(Z) are both Gaussian.
+
+    All inputs are expected to have a batch dimension.
+    """
+    obs_dim = observation.shape[0]
+    num_features = q_A_mean.shape[0]
+
+    sigma_obs_squared = sigma_obs * sigma_obs
+
+    # -0.5 * log (2 * pi * |sigma_o^2 I|)
+    term_one = -0.5 * torch.log(
+        2 * np.pi * torch.linalg.det(sigma_obs_squared * torch.eye(obs_dim)))
+
+    # o^T o
+    term_two = torch.inner(observation, observation)
+
+    # -2. * E[z_n] E[A_n]^T o
+    term_three = -2. * torch.einsum(
+        'b, bd, d->',
+        q_Z_mean,
+        q_A_mean,
+        observation)
+    term_three_check = -2. * torch.sum(torch.multiply(q_Z_mean,
+                                                      torch.matmul(q_A_mean,
+                                                                   observation)))
+    assert torch.isclose(term_three, term_three_check)
+
+    # \sum_k b_{nk} Tr[Sigma_{nk} + \mu_{nk} \mu_{nk}^T]
+    term_four = torch.einsum(
+        'b,b->',
+        q_Z_mean,
+        torch.einsum('bii->b', q_A_cov + torch.einsum('bi, bj->bij',
+                                                      q_A_mean,
+                                                      q_A_mean)))
+
+    # \sum_{k, k': k \neq k'} b_{nk} b_{nk'} \mu_{nk}^T \mu_{nk'}]
+    # I don't know how to exclude the k=k' elements in the double sum, so we'll subtract
+    # those using another einsum
+    term_five_all_pairs = torch.einsum(
+        'i,j,ik,jk->ij',
+        q_Z_mean,
+        q_Z_mean,
+        q_A_mean,
+        q_A_mean)
+    term_five_self_pairs = torch.trace(term_five_all_pairs)
+    term_five = torch.sum(term_five_all_pairs) - term_five_self_pairs
+
+    term_five_all_pairs_sum_check = torch.sum(torch.stack(
+        [q_Z_mean[k] * q_Z_mean[kprime] * torch.inner(q_A_mean[k], q_A_mean[kprime])
+         for k in range(num_features)
+         for kprime in range(num_features)]))
+    assert torch.isclose(torch.sum(term_five_all_pairs), term_five_all_pairs_sum_check)
+
+    total = term_one - 0.5 * (term_two + term_three + term_four + term_five) / sigma_obs_squared
+    return total
 
 
 def torch_entropy_bernoulli(probs: torch.Tensor) -> torch.Tensor:
+    """
+    Compute entropy of p(x) = Bernoulli(prob).
+    
+    All inputs are expected to have a batch dimension.
+    """
     one_minus_probs = 1. - probs
     log_odds = torch.log(torch.divide(probs, one_minus_probs))
-    entropy = -1. * torch.sum(torch.add(torch.multiply(probs,
-                                                     log_odds),
-                                      torch.log(one_minus_probs)))
+
+    # 0 * log 0 should defined as 0, but torch will set equal to NaN
+    # we consequently need to mask
+    probs_time_log_odds = torch.multiply(probs, log_odds)
+    probs_time_log_odds[torch.isclose(probs, torch.zeros_like(probs))] = 0.
+    entropy = -torch.sum(torch.add(probs_time_log_odds,
+                                   torch.log(one_minus_probs)))
     assert entropy >= 0.
     return entropy
 
 
 def torch_entropy_gaussian(mean: torch.Tensor,
                            cov: torch.Tensor) -> torch.Tensor:
+    """
+    Compute entropy of p(x) = Gaussian(mean, cov).
+
+    All inputs are expected to have a batch dimension.
+    """
+
     dim = mean.shape[-1]
 
     entropy = 0.5 * torch.sum(
@@ -141,4 +245,3 @@ def torch_logits_to_probs(logits):
 def torch_probs_to_logits(probs):
     logits = - torch.log(1. / probs - 1.)
     return logits
-
--- utils/inference.py
+++ utils/inference.py
@@ -16,10 +16,11 @@ inference_alg_strs = [
 def create_new_feature_params_multivariate_normal(torch_observation: torch.Tensor,
                                                   dish_eating_prior: torch.Tensor,
                                                   obs_idx: int,
-                                                  likelihood_params: Dict[str, torch.Tensor]):
+                                                  likelihood_params: Dict[str, torch.Tensor],
+                                                  sigma_obs_var: int = 1.):
     # data is necessary to not break backprop
     # see https://stackoverflow.com/questions/53819383/how-to-assign-a-new-value-to-a-pytorch-variable-without-breaking-backpropagation
-    assert_torch_no_nan_no_inf(torch_observation)
+    utils.helpers.torch_assert_no_nan_no_inf(torch_observation)
     max_num_features = likelihood_params['means'].shape[0]
     obs_dim = torch_observation.shape[0]
 
@@ -66,8 +67,9 @@ def recursive_ibp(observations,
 
     # Note: the expected number of latents grows logarithmically as a*b*log(b + T - 1)
     # The 10 is a hopefully conservative heuristic to preallocate.
-    max_num_features = 10 * int(inference_params['alpha'] * inference_params['beta'] * \
-                                np.log(inference_params['beta'] + num_obs - 1))
+    # TODO: reset to 10
+    max_num_features = 3 * int(inference_params['alpha'] * inference_params['beta'] * \
+                               np.log(inference_params['beta'] + num_obs - 1))
 
     # The recursion does not require recording the full history of priors/posteriors
     # but we record the full history for subsequent analysis.
@@ -131,26 +133,18 @@ def recursive_ibp(observations,
         # create_new_feature_params_fn = create_new_cluster_params_dirichlet_multinomial
         # posterior_fn = likelihood_dirichlet_multinomial
     elif likelihood_model == 'multivariate_normal':
-        # if likelihood_known:
-        #     assert likelihood_params['mean'].shape == (max_num_features, obs_dim)
-        #     assert likelihood_params['cov'].shape == (obs_dim, obs_dim)
-        #     # torch.from_numpy implicitly sets requires_grad to False
-        #     likelihood_params = dict(
-        #         mean=torch.from_numpy(likelihood_params['mean']),
-        #         cov=torch.from_numpy(likelihood_params['cov'])
-        #     )
-        # else:
         # dict mapping variables to variational parameters
-        A_cov = torch.stack([torch.eye(obs_dim, obs_dim)
-                           for _ in range((num_obs + 1) * max_num_features)])
-        A_cov = A_cov.view(num_obs + 1, max_num_features, obs_dim, obs_dim)
-        A_cov.requires_grad = True
+        # we use half covariance because we want to numerically optimize
+        A_half_cov = torch.stack([torch.eye(obs_dim, obs_dim)
+                                  for _ in range((num_obs + 1) * max_num_features)])
+        A_half_cov = A_half_cov.view(num_obs + 1, max_num_features, obs_dim, obs_dim)
+        A_half_cov.requires_grad = True
         variable_variational_params = dict(
             Z=dict(  # variational parameters for binary indicators
                 params=dict(
                     prob=torch.full(
                         size=(num_obs + 1, max_num_features),
-                        fill_value=0.5,
+                        fill_value=np.nan,
                         dtype=torch.float64,
                         requires_grad=False)),
                 optimize_fns=dict(
@@ -159,16 +153,16 @@ def recursive_ibp(observations,
             ),
             A=dict(  # variational parameters for Gaussian features
                 params=dict(
-                    # mean=torch.full(
-                    #     size=(num_obs + 1, max_num_features, obs_dim),
-                    #     fill_value=0.,
-                    #     dtype=torch.float64,
-                    #     requires_grad=True),
-                    mean=torch.from_numpy(
-                        np.random.normal(size=(num_obs + 1, max_num_features, obs_dim)),
-                        # requires_grad=True
-                    ),
-                    cov=A_cov),
+                    mean=torch.full(
+                        size=(num_obs + 1, max_num_features, obs_dim),
+                        fill_value=0.,
+                        dtype=torch.float64,
+                        requires_grad=True),
+                    # mean=torch.from_numpy(
+                    #     np.random.normal(size=(num_obs + 1, max_num_features, obs_dim)),
+                    #     # requires_grad=True
+                    # ),
+                    half_cov=A_half_cov),
                 optimize_fns=dict(),
             )
         )
@@ -176,7 +170,6 @@ def recursive_ibp(observations,
         optimizer = torch.optim.SGD(
             params=variable_variational_params['A']['params'].values(),
             lr=1.)
-
     else:
         raise NotImplementedError
 
@@ -205,73 +198,67 @@ def recursive_ibp(observations,
         # record latent prior
         dish_eating_priors[obs_idx, :] = dish_eating_prior.clone()
 
+        # initialize dish eating posterior to dish eating prior, before beginning inference
+        variable_variational_params['Z']['params']['prob'][obs_idx, :] = dish_eating_prior.clone()
+        dish_eating_posteriors.data[obs_idx, :] = dish_eating_prior.clone()
+
+        # initialize features to previous features as starting point for optimization
+        # . Use .data to not break backprop
+        for param_name, param_tensor in variable_variational_params['A']['params'].items():
+            param_tensor.data[obs_idx, :] = param_tensor.data[obs_idx - 1, :]
+
         for vi_idx in range(num_vi_steps):
 
-            # TODO: optimize Gaussian params
-            loss = recursive_ibp_compute_approx_lower_bound(
-                torch_observation=torch_observation,
-                obs_idx=obs_idx,
-                dish_eating_prior=dish_eating_prior,
-                variable_variational_params=variable_variational_params
-            )
+            # # if first observation, use closed form expression for features A
+            # if obs_idx == 1:
+            #     raise NotImplementedError
+            # else:  # otherwise, use gradient ascent on features A
 
-            loss.backward()
-
-            # instead of typical dynamics:
-            #       p_k <- p_k + (obs - p_k) / number of obs assigned to kth cluster
-            # we use the new dynamics
-            #       p_k <- p_k + posterior(obs belongs to kth cluster) * (obs - p_k) / total mass on kth cluster
-            # that effectively means the learning rate should be this scaled_prefactor
-            scaled_learning_rate = learning_rate * torch.divide(
-                dish_eating_posteriors[obs_idx, :],
-                dish_eating_posteriors_running_sum[obs_idx, :]) / num_vi_steps
-            scaled_learning_rate[torch.isnan(scaled_learning_rate)] = 0.
-            scaled_learning_rate[torch.isinf(scaled_learning_rate)] = 0.
-
-            # don't update the newest cluster
-            scaled_learning_rate[obs_idx] = 0.
-
-            for param_descr, param_tensor in likelihood_params.items():
-                # the scaled learning rate has shape (num latents,) aka (num obs,)
-                # we need to create extra dimensions of size 1 for broadcasting to work
-                # because param_tensor can have variable number of dimensions e.g. (num obs, obs dim)
-                # for mean vs (num obs, obs dim, obs dim) for covariance, we need to dynamically
-                # add the corect number of dimensions
-                reshaped_scaled_learning_rate = scaled_learning_rate.view(
-                    [scaled_learning_rate.shape[0]] + [1 for _ in range(len(param_tensor.shape[1:]))])
-                if param_tensor.grad is None:
-                    continue
-                else:
-                    scaled_param_tensor_grad = torch.multiply(
-                        reshaped_scaled_learning_rate,
-                        param_tensor.grad)
-                    param_tensor.data += scaled_param_tensor_grad
-                    utils.helpers.assert_torch_no_nan_no_inf(param_tensor.data[:obs_idx + 1])
-
-            recursive_ibp_optimize_bernoulli_variables(
+            # maximize approximate lower bound with respect to Z's parameters
+            Z_probs = recursive_ibp_optimize_bernoulli_variables(
                 torch_observation=torch_observation,
                 obs_idx=obs_idx,
                 dish_eating_prior=dish_eating_prior,
                 variable_variational_params=variable_variational_params)
+            variable_variational_params['Z']['params']['prob'][obs_idx, :] = Z_probs
+            # save in posteriors
+            dish_eating_posteriors.data[obs_idx, :] = variable_variational_params['Z'][
+                                                          'params']['prob'][obs_idx, :].clone()
 
+            # maximize approximate lower bound with respect to A's parameters
             optimizer.zero_grad()
-
-            # E step: infer posteriors using parameters
-            posterior_fn(
+            approx_lower_bound = recursive_ibp_compute_approx_lower_bound(
                 torch_observation=torch_observation,
-                likelihood_params=likelihood_params)
-            assert torch.all(~torch.isnan(likelihoods_per_latent[:obs_idx + 1]))
-            assert torch.all(~torch.isnan(log_likelihoods_per_latent[:obs_idx + 1]))
+                obs_idx=obs_idx,
+                dish_eating_prior=dish_eating_prior,
+                variable_variational_params=variable_variational_params)
+            approx_lower_bound.backward()
 
-            unnormalized_table_assignment_posterior = torch.multiply(
-                likelihoods_per_latent.detach(),
-                dish_eating_prior)
-            table_assignment_posterior = unnormalized_table_assignment_posterior / torch.sum(
-                unnormalized_table_assignment_posterior)
-            assert torch.allclose(torch.sum(table_assignment_posterior), one_tensor)
+            # scale learning rate by posterior(A_k) / sum_n prev_posteriors(A_k)
+            # scale by 1/num_vi_steps so that after num_vi_steps, we've moved O(1)
+            scaled_learning_rate = learning_rate * torch.divide(
+                dish_eating_posteriors[obs_idx, :],
+                dish_eating_posteriors[obs_idx, :] + dish_eating_posteriors_running_sum[obs_idx - 1, :]) / num_vi_steps
+            scaled_learning_rate[torch.isnan(scaled_learning_rate)] = 0.
+            scaled_learning_rate[torch.isinf(scaled_learning_rate)] = 0.
 
-            # record latent posterior
-            dish_eating_posteriors[obs_idx, :len(table_assignment_posterior)] = table_assignment_posterior
+            for var_name, var_dict in variable_variational_params.items():
+                for param_descr, param_tensor in var_dict['params'].items():
+                    # the scaled learning rate has shape (num latents,) aka (num obs,)
+                    # we need to create extra dimensions of size 1 for broadcasting to work
+                    # because param_tensor can have variable number of dimensions e.g. (num obs, obs dim)
+                    # for mean vs (num obs, obs dim, obs dim) for covariance, we need to dynamically
+                    # add the corect number of dimensions
+                    reshaped_scaled_learning_rate = scaled_learning_rate.view(
+                        [scaled_learning_rate.shape[0]] + [1 for _ in range(len(param_tensor.shape[1:]))])
+                    if param_tensor.grad is None:
+                        continue
+                    else:
+                        scaled_param_tensor_grad = torch.multiply(
+                            reshaped_scaled_learning_rate,
+                            param_tensor.grad)
+                        param_tensor.data += scaled_param_tensor_grad
+                        utils.helpers.torch_assert_no_nan_no_inf(param_tensor.data[:obs_idx + 1])
 
             # update running sum of posteriors
             dish_eating_posteriors_running_sum[obs_idx, :] = torch.add(
@@ -280,11 +267,9 @@ def recursive_ibp(observations,
             assert torch.allclose(torch.sum(dish_eating_posteriors_running_sum[obs_idx, :]),
                                   torch.Tensor([obs_idx + 1]).double())
 
-            # M step: update parameters
-            # Note: log likelihood is all we need for optimization because
-            # log p(x, z; params) = log p(x|z; params) + log p(z)
-            # and the second is constant w.r.t. params gradient
-            loss = torch.mean(log_likelihoods_per_latent)
+            # record dish-eating posterior
+            dish_eating_posteriors[obs_idx, :] = variable_variational_params['Z'][
+                                                     'params']['prob'][obs_idx, :].clone()
 
         # update how many dishes have been sampled
         non_eaten_dishes_posteriors_running_prod[obs_idx, :] = np.multiply(
@@ -319,23 +304,33 @@ def recursive_ibp_compute_approx_lower_bound(torch_observation,
                                              obs_idx,
                                              dish_eating_prior,
                                              variable_variational_params):
-    indicators_term = utils.helpers.torch_expected_log_bernoulli_prob(
+    # convert half covariances to covariances
+    prior_A_cov = utils.helpers.torch_convert_half_cov_to_cov(
+        half_cov=variable_variational_params['A']['params']['half_cov'][obs_idx - 1])
+    posterior_A_cov = utils.helpers.torch_convert_half_cov_to_cov(
+        half_cov=variable_variational_params['A']['params']['half_cov'][obs_idx])
+
+    indicators_term = utils.helpers.torch_expected_log_bernoulli_under_bernoulli(
         p_prob=dish_eating_prior,
         q_prob=variable_variational_params['Z']['params']['prob'][obs_idx])
-    gaussian_term = utils.helpers.torch_expected_log_gaussian_prob(
+    gaussian_term = utils.helpers.torch_expected_log_gaussian_under_gaussian(
         p_mean=variable_variational_params['A']['params']['mean'][obs_idx - 1],
-        p_cov=variable_variational_params['A']['params']['cov'][obs_idx - 1],
+        p_cov=prior_A_cov,
         q_mean=variable_variational_params['A']['params']['mean'][obs_idx],
-        q_cov=variable_variational_params['A']['params']['cov'][obs_idx])
-    likelihood_term = utils.helpers.torch_expected_log_likelihood(
-        torch_observation=torch_observation
-    )
+        q_cov=posterior_A_cov)
+    likelihood_term = utils.helpers.torch_expected_log_gaussian_under_linear_gaussian(
+        observation=torch_observation,
+        q_A_mean=variable_variational_params['A']['params']['mean'][obs_idx],
+        q_A_cov=posterior_A_cov,
+        q_Z_mean=variable_variational_params['Z']['params']['prob'][obs_idx])
     bernoulli_entropy = utils.helpers.torch_entropy_bernoulli(
         probs=variable_variational_params['Z']['params']['prob'][obs_idx])
     gaussian_entropy = utils.helpers.torch_entropy_gaussian(
         mean=variable_variational_params['A']['params']['mean'][obs_idx],
-        cov=variable_variational_params['A']['params']['cov'][obs_idx])
-    lower_bound = indicators_term + gaussian_term + bernoulli_entropy + gaussian_entropy
+        cov=posterior_A_cov)
+
+    lower_bound = indicators_term + gaussian_term + likelihood_term + bernoulli_entropy + gaussian_entropy
+
     return lower_bound
 
 
@@ -343,16 +338,71 @@ def recursive_ibp_optimize_bernoulli_variables(torch_observation: torch.Tensor,
                                                obs_idx: int,
                                                dish_eating_prior: torch.Tensor,
                                                variable_variational_params: Dict[str, dict],
-                                               var_name: str = 'Z'):
-
-    term_to_exponentiate = torch.zeros_like(
-        variable_variational_params[var_name]['params']['prob'][obs_idx, :])
+                                               var_name: str = 'Z',
+                                               sigma_obs_squared: int = 1.):
+    # term_to_exponentiate = torch.zeros_like(
+    #     variable_variational_params[var_name]['params']['prob'][obs_idx, :])
     log_bernoulli_prior_term = torch.log(torch.divide(dish_eating_prior, 1. - dish_eating_prior))
-    term_to_exponentiate += log_bernoulli_prior_term
-    for k in range(term_to_exponentiate.size()[0]):
-        # log_gaussian_term = -2 * torch.dot(variable_variational_params[])
-        print(10)
-    raise NotImplementedError
+    A_cov = utils.helpers.torch_convert_half_cov_to_cov(
+        variable_variational_params['A']['params']['half_cov'][obs_idx, :])
+
+    # -2 mu_{nk}^T o_n
+    term_one = -2. * torch.einsum(
+        'bk,k->b',
+        variable_variational_params['A']['params']['mean'][obs_idx, :],
+        torch_observation)
+
+    # Tr[\Sigma_{nk} + \mu_{nk} \mu_{nk}^T]
+    term_two = torch.einsum(
+        'bii->b',
+        torch.add(A_cov,
+                  torch.einsum('bi,bj->bij',
+                               variable_variational_params['A']['params']['mean'][obs_idx, :],
+                               variable_variational_params['A']['params']['mean'][obs_idx, :])))
+
+    # 2 \mu_{nk}^T (\sum_{k': k' \neq k} b_{nk'} \mu_{nk'})
+    # = 2 \mu_{nk}^T (\sum_{k'} b_{nk'} \mu_{nk'}) - 2 b_{nk} \mu_{nk}^T \mu_{nk}
+    term_three_all_pairs = torch.einsum(
+        'bi, i->b',
+        variable_variational_params['A']['params']['mean'][obs_idx, :],
+        torch.einsum(
+            'b, bi->i',
+            variable_variational_params['Z']['params']['prob'][obs_idx, :],
+            variable_variational_params['A']['params']['mean'][obs_idx, :]))
+    term_three_self_pairs = torch.einsum(
+            'b,bk,bk->b',
+            variable_variational_params['Z']['params']['prob'][obs_idx, :],
+            variable_variational_params['A']['params']['mean'][obs_idx, :],
+            variable_variational_params['A']['params']['mean'][obs_idx, :])
+    term_three = 2. * (term_three_all_pairs - term_three_self_pairs)
+
+    num_features = dish_eating_prior.shape[0]
+    mu = variable_variational_params['A']['params']['mean'][obs_idx, :]
+    b = variable_variational_params['Z']['params']['prob'][obs_idx, :]
+    term_three_check = 2. * torch.stack([
+        torch.inner(mu[k],
+                    torch.sum(torch.stack([b[kprime] * mu[kprime]
+                                           for kprime in range(num_features)
+                                           if kprime != k]),
+                              dim=0))
+        for k in range(num_features)
+    ])
+    assert torch.allclose(term_three, term_three_check)
+
+    term_to_exponentiate = log_bernoulli_prior_term - 0.5 * (term_one + term_two + term_three) / sigma_obs_squared
+    bernoulli_probs = 1. / (1. + torch.exp(-term_to_exponentiate))
+
+    import matplotlib.pyplot as plt
+    plt.plot(dish_eating_prior, label='prior')
+    plt.plot(bernoulli_probs.detach().numpy(), label='posterior')
+    plt.legend()
+    plt.show()
+
+    # check that Bernoulli probs are all valid
+    assert torch.all(0. <= bernoulli_probs)
+    assert torch.all(bernoulli_probs <= 1.)
+
+    return bernoulli_probs
 
 
 def posterior_multivariate_normal_linear_regression_simultaneous(torch_observation,
